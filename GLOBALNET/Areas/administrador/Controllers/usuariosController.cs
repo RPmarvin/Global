0ce3060b8c526e739fae578c5aa709cdcae9e7e4
RPmarvin <30728701+RPmarvin@users.noreply.github.com>
2017-08-28 01:53:18 -0500

    agrega area administrador

---
+++ GLOBALNET/Areas/administrador/Controllers/usuariosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GLOBALNET.Models;
+
+namespace GLOBALNET.Areas.administrador.Controllers
+{
+    public class usuariosController : Controller
+    {
+        private CajabdEntities db = new CajabdEntities();
+
+        public ActionResult Huanta()
+        {
+            return View();
+        }
+        public ActionResult SanFransisco()
+        {
+            return View();
+        }
+        // GET: administrador/usuarios
+        public ActionResult Index()
+        {
+            return View(db.usuario.ToList());
+        }
+
+        // GET: administrador/usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: administrador/usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: administrador/usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_usuario,nombre_usuario,apellido_usuario,dni_usuario,cargo_usuario,Sede_usuario")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        // GET: administrador/usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: administrador/usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_usuario,nombre_usuario,apellido_usuario,dni_usuario,cargo_usuario,Sede_usuario")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: administrador/usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: administrador/usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            usuario usuario = db.usuario.Find(id);
+            db.usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+}
---
+++ GLOBALNET/Areas/administrador/administradorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace GLOBALNET.Areas.administrador
+{
+    public class administradorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "administrador";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "administrador_default",
+                "administrador/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
