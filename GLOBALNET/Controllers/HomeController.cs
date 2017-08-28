60f076d3f9e9e9a7cc22cb2001c86ae536bae044
fairofox <fairo@hotmail.com>
2017-08-27 20:04:51 -0500

    reproteprueba1;

--- GLOBALNET/Controllers/HomeController.cs
+++ GLOBALNET/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace GLOBALNET.Controllers
         {
             return View();
         }
+        public ActionResult ReporteDetallado()
+        {
+            return View();
+        }
+
     }
 }
