04bdf8d4e8892e16a6ba81e655cda18335f084fb
RPmarvin <30728701+RPmarvin@users.noreply.github.com>
2017-09-02 19:39:20 -0500

    sabado

--- GLOBALNET/Controllers/HomeController.cs
+++ GLOBALNET/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace GLOBALNET.Controllers
 {
     public class HomeController : Controller
     {
+        private Models.CajabdEntities bd = new Models.CajabdEntities();
         // GET: Home
         public ActionResult Index()
         {
@@ -29,6 +30,32 @@ namespace GLOBALNET.Controllers
         {
             return View();
         }
+        //metodo para agregar sesiones 
+        public ActionResult Login(string usuario, string clave)
+        {
+            var u = bd.usuario.FirstOrDefault(x => x.nombre_usuario== usuario && x.dni_usuario== clave);
+            if (u != null)
+            {
+                Helper.SessionHelper.AddUserToSession(u.id_usuario.ToString());
+            }
+            return RedirectToAction("Caja", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Helper.SessionHelper.DestroyUserSession();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public static string ObtenerNombreUsuario()
+        {
+            using (var b = new Models.CajabdEntities())
+            {
+                return b.usuario.Find(Helper.SessionHelper.GetUser()).nombre_usuario;
+            }
+        }
+
 
     }
 }
